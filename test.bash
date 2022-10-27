8f127780a2a2d5a4923492140a3567aa73e4737b
Musashi Harada <m08022956340@gmail.com>
2022-10-27 12:40:44 +0900

    Add a test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 musashiharada 　　　　　
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,6 +12,15 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]     || ng ${LINENO}
+[ "${out} = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]     || ng ${LINENO}
+[ "${out} = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
