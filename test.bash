1b1ee24502676c85df5ef109c103c91a5a0a9fad
Musashi Harada <m08022956340@gmail.com>
2022-10-27 12:27:40 +0900

    Add a test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,20 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 musashiharada 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
+
 
